b9e4b9f379f7e2b74f132020a76b0dfa23cfd9f3
Aman <amanmiah@outlook.com>
2024-10-12 10:39:09 +0100

    Task 1A Completed Code

---
+++ Coursework/main.py
@@ -0,0 +1,29 @@
+from Coursework.Task1CLRS.floyd_warshall import floyd_warshall
+import numpy as np
+
+NA = float("inf") # For nodes (Stations) that have no connections with each other.
+
+"""
+------------------------------
+Train Station Connections    - 
+------------------------------
+    A     B     C     D     E -
+A   0     6     ∞    12     ∞ -
+B   6     0     4     ∞     ∞ -
+C   ∞     4     0     8     ∞ -
+D   12    ∞     8     0     3 -
+E   ∞     ∞     ∞     3     0 -
+------------------------------
+"""
+
+train_stations = np.array([
+    [0,6,NA,12,NA], # Station: A
+    [6,0,4,NA,NA], # Station: B
+    [NA,4,0,8,NA], # Station: C
+    [12,NA,8,0,3], # Station: D
+    [NA,NA,NA,3,0] # Station: E
+])
+
+shortest_paths = floyd_warshall(train_stations,len(train_stations))
+
+print(shortest_paths)
